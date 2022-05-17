1c7be19b753a5a5e083674fd7d0f1d1851f296a1
Murylev <96014353+Murylev@users.noreply.github.com>
2022-05-18 00:15:50 +0500

    Telegram_Bot_Convert_Many

---
+++ Config.py
@@ -0,0 +1,10 @@
+TOKEN = '5340543368:AAHCyAnnvp70rYWB9GfiBfPM_o0xi5dmV68'
+keys = {
+    'Юань': 'CNY',
+    'Йена': 'JPY',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Франк': 'CHF',
+    'Лира': 'TRY',
+    'Рубль': 'RUB',
+}
